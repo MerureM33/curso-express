5e50d4a5ff6d1d53dbbb3309e27014dade7e5f27
MerureM33 <mefisto.edm@gmail.com>
2025-09-02 16:16:57 -0500

    Agregando script start para Railway5

--- app.js
+++ app.js
@@ -9,31 +9,32 @@ const categorias = [
   {
     nombre: "Programación",
     cursos: [
-      { id: 1, titulo: "Curso de JavaScript", descripcion: "Aprende JS desde cero", imagen: "/images/cursoJavascript.jpg" },
-      { id: 2, titulo: "Curso de Node.js", descripcion: "Crea apps con Node y Express", imagen: "/images/cursonodejs.jpg" },
-      { id: 3, titulo: "Curso de Python", descripcion: "Fundamentos de Python", imagen: "/images/cursoPython.jpg" },
-      { id: 4, titulo: "Curso de Java", descripcion: "POO y aplicaciones con Java", imagen: "/images/cursoJava.jpg" }
+      { id: 1, titulo: "Curso de JavaScript", descripcion: "Aprende JS desde cero", imagen: "/images/cursoJavascript.jpg", precio: "$ 15" },
+      { id: 2, titulo: "Curso de Node.js", descripcion: "Crea apps con Node y Express", imagen: "/images/cursonodejs.jpg", precio: "$ 15" },
+      { id: 3, titulo: "Curso de Python", descripcion: "Fundamentos de Python", imagen: "/images/cursoPython.jpg", precio: "$ 15" },
+      { id: 4, titulo: "Curso de Java", descripcion: "POO y aplicaciones con Java", imagen: "/images/cursoJava.jpg", precio: "$ 15" }
     ]
   },
   {
     nombre: "Diseño",
     cursos: [
-      { id: 5, titulo: "Curso de Photoshop", descripcion: "Retoque digital básico", imagen: "/images/cursoPhotoshop.jpg" },
-      { id: 6, titulo: "Curso de Illustrator", descripcion: "Diseño vectorial", imagen: "/images/cursoIllustrador.jpg" },
-      { id: 7, titulo: "Curso de UX/UI", descripcion: "Diseño de interfaces", imagen: "/images/cursoUX-UI.jpg" },
-      { id: 8, titulo: "Curso de Figma", descripcion: "Prototipado y diseño web", imagen: "/images/cursoFigma.jpg" }
+      { id: 5, titulo: "Curso de Photoshop", descripcion: "Retoque digital básico", imagen: "/images/cursoPhotoshop.jpg", precio: "$ 15" },
+      { id: 6, titulo: "Curso de Illustrator", descripcion: "Diseño vectorial", imagen: "/images/cursoIllustrador.jpg", precio: "$ 15" },
+      { id: 7, titulo: "Curso de UX/UI", descripcion: "Diseño de interfaces", imagen: "/images/cursoUX-UI.jpg", precio: "$ 15" },
+      { id: 8, titulo: "Curso de Figma", descripcion: "Prototipado y diseño web", imagen: "/images/cursoFigma.jpg", precio: "$ 15" }
     ]
   },
   {
     nombre: "Marketing",
     cursos: [
-      { id: 9, titulo: "Curso de Marketing Digital", descripcion: "Estrategias digitales", imagen: "/images/cursoMarketingDigital.jpg" },
-      { id: 10, titulo: "Curso de SEO", descripcion: "Posiciona tu web en Google", imagen: "/images/cursoSEO.jpg" },
-      { id: 11, titulo: "Curso de Redes Sociales", descripcion: "Gestión de Instagram y Facebook", imagen: "/images/cursoRedesSociales.jpg" },
-      { id: 12, titulo: "Curso de Publicidad", descripcion: "Google Ads y campañas online", imagen: "/images/cursoPublicidad.jpg" }
+      { id: 9, titulo: "Curso de Marketing Digital", descripcion: "Estrategias digitales", imagen: "/images/cursoMarketingDigital.jpg", precio: "$ 15" },
+      { id: 10, titulo: "Curso de SEO", descripcion: "Posiciona tu web en Google", imagen: "/images/cursoSEO.jpg", precio: "$ 15" },
+      { id: 11, titulo: "Curso de Redes Sociales", descripcion: "Gestión de Instagram y Facebook", imagen: "/images/cursoRedesSociales.jpg", precio: "$ 15" },
+      { id: 12, titulo: "Curso de Publicidad", descripcion: "Google Ads y campañas online", imagen: "/images/cursoPublicidad.jpg", precio: "$ 15" }
     ]
   }
 ];
+
 
 app.get("/",(req,res)=>{
     res.render("index")
